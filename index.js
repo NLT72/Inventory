c02d5c3d842f129d6850a42fc90771cf37e1a519
Ritk21 <77534764+NLT72@users.noreply.github.com>
2024-09-18 16:37:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import ProductsController from './src/controllers/product.controller.js';
+import ejsLayouts from 'express-ejs-layouts';
+import path from 'path';
+import validationMiddleware from './src/middlewares/validation.middleware.js';
+
+const app = express();
+const productsController =
+  new ProductsController();
+app.use(express.static('Public'));
+app.use(ejsLayouts);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set(
+  'views',
+  path.join(path.resolve(), 'src', 'views')
+);
+
+app.get('/', productsController.getProducts);
+app.get(
+  '/add-product',
+  productsController.getAddProduct
+);
+app.get(
+  '/update-product/:id',
+  productsController.getUpdateProductView
+);
+app.post(
+  '/delete-product/:id',
+   productsController.getdeleteproduct
+  );
+app.post(
+  '/',
+  validationMiddleware,
+  productsController.postAddProduct
+);
+app.post(
+  '/update-product',
+  productsController.getPostUpdate
+);
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
